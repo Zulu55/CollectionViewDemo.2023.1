094a8b8c96018f91e96f3ee2c65b1e92df742267
Juan Zuluaga <Juan.Zuluaga_EX@kdrp.com>
2023-04-14 09:45:59 -0500

    Initial commit

---
+++ CollectionViewDemo/MVVM/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CollectionViewDemo.MVVM.Models
+{
+	public class Product
+	{
+		public string Name { get; set; }
+
+		public decimal Price { get; set; }
+
+		public string Image { get; set; }
+
+		public int Stock { get; set; }
+
+		public bool HasOffer { get; set; }
+
+		public decimal OfferPrice { get; set; }
+	}
+}
+
---
+++ CollectionViewDemo/MVVM/Views/DataView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using CollectionViewDemo.MVVM.ViewModels;
+
+namespace CollectionViewDemo.MVVM.Views;
+
+public partial class DataView : ContentPage
+{
+	public DataView()
+	{
+		InitializeComponent();
+		BindingContext = new DataViewModel();
+	}
+}
