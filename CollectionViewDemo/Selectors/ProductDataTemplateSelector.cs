074587dc264362581de87b432e5ba6a50324e946
Juan Zuluaga <Juan.Zuluaga_EX@kdrp.com>
2023-04-14 10:40:04 -0500

    Introducing data template

---
+++ CollectionViewDemo/Selectors/ProductDataTemplateSelector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CollectionViewDemo.MVVM.Models;
+namespace CollectionViewDemo.Selectors
+{
+	public class ProductDataTemplateSelector : DataTemplateSelector
+	{
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var product = item as Product;
+            if (!product.HasOffer)
+            {
+                Application.Current.Resources.TryGetValue("ProductStyle", out var productStyle);
+                return productStyle as DataTemplate;
+            }
+            return new DataTemplate();
+        }
+    }
+}
+
